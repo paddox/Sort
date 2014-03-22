4e2e8a811f8ecc5c6008c9ccf171c04d24dac38a
paddox <paddox@yandex.ru>
2014-03-22 21:42:42 +0400

    Папка Doxygen

--- html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['puzirek',['puzirek',['../_main_8cpp.html#ae0c6f2ea2c4adb9a9d78b38038beba40',1,'puzirek(int n, char *list):&#160;Sortirovka_puzirkom.cpp'],['../_sortirovka__puzirkom_8cpp.html#ae0c6f2ea2c4adb9a9d78b38038beba40',1,'puzirek(int n, char *list):&#160;Sortirovka_puzirkom.cpp']]]
-];
--- html/search/files_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['sortirovka_5fpuzirkom_2ecpp',['Sortirovka_puzirkom.cpp',['../_sortirovka__puzirkom_8cpp.html',1,'']]],
-  ['sortirovka_5fsliyaniyem_2ecpp',['Sortirovka_sliyaniyem.cpp',['../_sortirovka__sliyaniyem_8cpp.html',1,'']]],
-  ['stdafx_2ecpp',['stdafx.cpp',['../stdafx_8cpp.html',1,'']]],
-  ['stdafx_2eh',['stdafx.h',['../stdafx_8h.html',1,'']]]
-];
